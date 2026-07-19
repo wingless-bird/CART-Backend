128d027b5429052aa0bdb82fe90e4d79b7b54824
wingless-bird <Siddique.mansoor164@gmail.com>
2026-07-19 20:58:10 +0500

    Fix Mongoose 9 pre-save hook and start script filename casing

--- models/Admin.js
+++ models/Admin.js
@@ -26,16 +26,15 @@ const adminSchema = new mongoose.Schema({
 // Only re-hashes if the password field was actually changed,
 // so updating the username alone won't double-hash it.
 // ==========================================================
-adminSchema.pre("save", async function (next) {
+adminSchema.pre("save", async function () {
 
     if (!this.isModified("password")) {
-        return next();
+        return;
     }
 
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 
-    next();
 });
 
 // ==========================================================
